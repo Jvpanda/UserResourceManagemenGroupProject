e17e23bda8ef147ef770f174f933500eb062071b
Cameron <58471843+CameronTheDeveloper@users.noreply.github.com>
2025-12-18 12:03:09 -0800

    Update Student.h

--- src/Student.h
+++ src/Student.h
@@ -2,19 +2,21 @@
 #define STUDENT_H
 
 #include "User.h"
+#include <iostream>
 
 class Student : public User
 {
+    private:
+        int id;
 
-        private:
-                int id;
+    public:
+        Student();
+        Student(const std::string &email, int id);
+        
+        int getID() const;
+        void setID(int id);
 
-        public:
-                Student();
-                Student(const std::string &email, int id);
-                int getID() const;
-                void setID(int id);
-
+        void displayMenu() const override;
 };
 
 #endif
