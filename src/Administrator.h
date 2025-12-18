e8b8518b69c8d7f828520c0008548402768b375e
Cameron <58471843+CameronTheDeveloper@users.noreply.github.com>
2025-12-18 13:07:25 -0800

    Update Administrator.h

--- src/Administrator.h
+++ src/Administrator.h
@@ -2,18 +2,21 @@
 #define ADMINISTRATOR_H
 
 #include "User.h"
+#include <iostream>
 
 class Administrator : public User
 {
+    private:
+        std::string password;
 
-        private:
-                std::string password;
+    public:
+        Administrator();
+        Administrator(const std::string &email, const std::string &password);
+        
+        const std::string &getPassword() const;
+        void setPassword(const std::string &password);
 
-        public:
-                Administrator();
-                Administrator(const std::string &email, const std::string &password);
-                const std::string &getPassword() const;
-                void setPassword(const std::string &password);
+        void displayMenu() const override;
 };
 
 #endif
